3bd9925362c3d9f5c9bb07c83048e37666ff6b31
Stefan Salituro <stefansalituro@gmail.com>
2020-06-28 15:50:28 -0400

    Eighth commit- last commit for Sunday likely as I have run out of time.. the index.html page is nearly complete with bootstrap and the css and javascript linked. The script.js has made good progress as the list of searched cities array has been declared.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var APIKey = "";
+
 // psuedo think of the variables that need to be created - eg. humidity, uv index, current temp, city, country etc..
 
 let country = "";
@@ -29,4 +31,41 @@ $(document).ready(function(){
     var prevCity = listOfSearchedCities[0];
     searchCity(prevCity);
   }
+});
+
+// The jQuery powered javascript has be created for the search button and the search input in the index.html 
+// must work with the parameters of the openWeatherAPI
+$("search-btn").on("click",function() {
+  event.preventDefault();
+  clearDisplayedWeatherInfo()
+  resetGlobalVariables()
+  // The val() method returns or sets the value attribute of the selected elements
+  var cityName = $("input").val().toUpperCase().trim();
+  $("#search-input").val("");
+  searchCity(cityName);
+}
+
+if (cityName !== "" && listOfSearchedCities[0] !== cityName) {
+  listOfSearchedCities.unshift(cityName);
+  localStorage.setItem("searched-cities", JSON.stringify(listOfSearchedCities));
+  if (listOfSearchedCities.length === 1) {
+    $("#searchedcities-card").removeClass("hide");
+  }
+
+  console.log($("ul#searchedcities-list a").length);
+  if ($("ul#searchedcities-list a").length >= 5) {
+      ($("ul#searchedcities-list a:eq(4").remove());
+  }
+  $("#searched-cities-list").prepend(`<a href="#" class="list-group-item" style="text-decoration: none; color: black;">
+    <li>${cityName}</li>
+    </a>`);
+}
+
+});
+// Reset the displayed weather info, the global variables and the searched cities
+$(document).on("click", ".list-group-item", function() {
+  var cityName = $(this).text();
+  clearDisplayedWeatherInfo();
+  resetGlobalVariables();
+  searchCity(cityName);
 });
