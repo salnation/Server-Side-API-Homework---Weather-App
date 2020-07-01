12921a8f2cecf98091f1bdf11da1a93a87c9d001
Stefan Salituro <stefansalituro@gmail.com>
2020-06-30 20:03:30 -0400

    Tuesday evening first commit - elements of the script.js have been altered and variables have been changed, added, and deleted.

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-var APIKey = "";
 
-// psuedo think of the variables that need to be created - eg. humidity, uv index, current temp, city, country etc..
-
-let country = "";
-let humidty = "";
-let city = "";
-let uvIndex = "";
-let longitude = "";
-let latitude = "";
-let windSpeed = "";
-let tempF = "";
-let tempC = "";
-let windSpeed = "";
+var city = "";
+var currentDate = "";
+var tempF = "";
+var humidtyValue = "";
+var windSpeed = "";
+var uvIndexValue = "";
+var latitude = "";
+var longitude = "";
+var minTempF = "";
+var maxTempF = "";
+var minTempC = "";
+var maxTempC = "";
 
 // Array of cities that have been previously been search stored in localStorage 
 var listOfSearchedCities = [];
 
 // get the list of searched cities from localStorage utilizing JSON and getItem from localStorage
-var getSearchedCities = JSON.parse(localStorage.getItem("searched-cities"));
-if (getSearchedCities !== null) {
-    getSearchedCities.array.forEach(function(city));
-    listOfSearchedCities = getSearchedCities;
+var getSearchedCitiesFromLS = JSON.parse(localStorage.getItem("searched-cities"));
+if (getSearchedCitiesFromLS !== null) {
+    getSearchedCitiesFromLS.forEach(function(city) {city.toUpperCase();
+    listOfSearchedCities = getSearchedCitiesFromLS;
     }
 
 // jQuery retrieve document and diplay the list of searched cities - jumpt to a recently searched for city 
 
 $(document).ready(function(){
   displayCities(listOfSearchedCities);
-  if (getSearchedCities !== null) {
-    var prevCity = listOfSearchedCities[0];
-    searchCity(prevCity);
+  if (getSearchedCitiesFromLS !== null) {
+    var lastCity = listOfSearchedCities[0];
+    searchCity(lastCity);
   }
 });
 
