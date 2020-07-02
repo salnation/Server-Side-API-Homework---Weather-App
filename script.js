84ae9bbb2bd38bc21be186ef78597d7a844f933a
Stefan Salituro <stefansalituro@gmail.com>
2020-07-02 18:52:41 -0400

    First of my final few commits for before homework is due on thursday - Removed several variables including the weather values in celcius variables which I had originally planned to use but will have to convert K instead.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
+// this is the API key the I was given when I registered with Open Weather API
+var APIKey = "dd6aa1dc7480a9dd05920f7b6ee1ffac"
 
 var city = "";
 var currentDate = "";
-var tempF = "";
-var humidtyValue = "";
-var windSpeed = "";
+var tempF = "";  //current temperature
+var humidtyValue = ""; 
+var windSpeed = ""; // current wind speed
 var uvIndexValue = "";
 var latitude = "";
 var longitude = "";
 var minTempF = "";
 var maxTempF = "";
-var minTempC = "";
-var maxTempC = "";
+var minTempK = ""; // Had to change celecius to K for conversion
+var maxTempK = "";
+
+//Next set of variables to add include humidity, and the weather icons and I forgot to add the variable to country
+
+var dayhumidity = "";
+var currentWeatherIconCode = "";
+var currentWeatherIconUrl = "";
+var iconcode = "";
+var iconurl = "";
+var country = "";
 
 // Array of cities that have been previously been search stored in localStorage 
+
 var listOfSearchedCities = [];
 
 // get the list of searched cities from localStorage utilizing JSON and getItem from localStorage
+
 var getSearchedCitiesFromLS = JSON.parse(localStorage.getItem("searched-cities"));
 if (getSearchedCitiesFromLS !== null) {
     getSearchedCitiesFromLS.forEach(function(city) {city.toUpperCase();
@@ -29,11 +42,14 @@ $(document).ready(function(){
   if (getSearchedCitiesFromLS !== null) {
     var lastCity = listOfSearchedCities[0];
     searchCity(lastCity);
+
   }
+
 });
 
 // The jQuery powered javascript has be created for the search button and the search input in the index.html 
 // must work with the parameters of the openWeatherAPI
+
 $("search-btn").on("click",function() {
   event.preventDefault();
   clearDisplayedWeatherInfo()
@@ -54,10 +70,13 @@ if (cityName !== ""&& listOfSearchedCities[0] !== cityName) {
   if ($("ul#searched-cities-list a").length >= 5) {
       ($("ul#searched-cities-list a:eq(4").remove());
   }
+
   $("#searched-cities-list").prepend(`<a href="#" class="list-group-item" style="text-decoration: none; color: black;">
     <li>${cityName}</li>
     </a>`);
+
 }
+
 });
 
 // Reset the displayed weather info, the global variables and the searched cities
@@ -68,7 +87,8 @@ $(document).on("click", ".list-group-item", function() {
   searchCity(cityName);
 });
 
-// adding the function for displayCurrentWeather - variables needed to be added to the function include the card heading, the weather image, the temperature, humidity, wind speed and uv index
+// adding the function for displayCurrentWeather - variables needed to be added to the function include the card heading, the weather image, the temperature, humidity, wind speed and uv index 
+// sets the path to attain the values for the following variables
 function displayCurrentWeather() {
   var cardDiv = $("<div class='container border bg-light'>");
   var weatherImage = $("<img>").attr('src', currentWeatherIconUrl);
@@ -78,13 +98,15 @@ function displayCurrentWeather() {
   var humidityEl = $("<p>").text("Humidity: " + humidityValue + "%");
   var windSpeedEl = $("<p>").text("Wind Speed: " + windSpeed + " MPH");
   var uvIndexEl = $("<p>").text("UV Index: ");
+  // variable I forgot to add** var uvIndexValueEl
+  var uvIndexValueEl = $("<span>").text(uvIndexValue).css("background-color", getColorCodeForUVIndex(uvIndexValue)).addClass("text-white");
   
+  uvIndexEl.append(uvIndexValueEl);
   cardDiv.append(cardHeader);
-  cardDiv.append(temperatureC);
-  cardDiv.append(temperatureF);
-  cardDiv.append(humidity);
-  cardDiv.append(windSpeed);
-  cardDiv.append(uvIndex);
+  cardDiv.append(temperatureEl);
+  cardDiv.append(humidityEl);
+  cardDiv.append(windSpeedEl);
+  cardDiv.append(uvIndexEl;
   $("#current-weather-conditions").append(cardDiv);
 }
 
@@ -95,17 +117,13 @@ var cardBlockDiv = $("<div>").attr("class", "card-block");
 var cardTitleDiv = $("<div>").attr("class", "card-block");
 var cardTitleHeader = $("<h6>").text(dateValue).addClass("pt-2");
 var cardTextDiv = $("<div>").attr("class", "card-text");
-var minTempF = $("<p>").text("Min Temp: " + minTempF + " ºF").css("font-size", "0.60rem");
-var maxTempF = $("<p>").text("Max Temp: " + maxTempF + " ºF").css("font-size", "0.60rem");
-var minTempC = $("<p>").text("Min Temp: " + minTempC + " ºC").css("font-size", "0.60rem");
-var maxTempC = $("<p>").text("Max Temp: " + maxTempC + " ºC").css("font-size", "0.60rem");
+var minTempEl = $("<p>").text("Min Temp: " + minTempF + " ºF").css("font-size", "0.60rem");
+  var maxTempEl = $("<p>").text("Max Temp: " + maxTempF + " ºF").css("font-size", "0.60rem");
 var humidityEl = $("<p>").text("Humidity: " + dayhumidity + "%").css("font-size", "0.60rem");
 
 cardTextDiv.append(imgEl);
-cardTextDiv.append(minTempF);
-cardTextDiv.append(maxTempF);
-cardTextDiv.append(minTempC;
-cardTextDiv.append(maxTempC);
+cardTextDiv.append(minTempEl;
+cardTextDiv.append(maxTempFEl;
 cardTextDiv.append(humidityEl);
 cardTitleDiv.append(cardTitleHeader);
 cardBlockDiv.append(cardTitleDiv);
